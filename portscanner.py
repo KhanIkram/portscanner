0c6a72c30f9556f1ae369ddd6c5b875db910880c
KhanIkram <ikramrkhan3@gmail.com>
2024-01-09 13:26:57 -0800

    Update portscanner.py
     v1.1 - 01/08/2024 - added comments and improved code.

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,13 @@
+# Author: Ikram Khan
+# v1.0 - 11/22/2023
+# v1.1 - 01/08/2024 - added comments and improved code. 
+
 import socket
 
 def scan_ports(target):
     # Display a banner for the start of the scan
+    # '40' multiplied by '*' creates a string consisting of 40 asterisk characters.
+    # F-string helps in creating a dynamic banner where the IP address being scanned ('target').
     print("*" * 40)
     print(f"* Scanning: {target} *")
     print("*" * 40)
@@ -11,19 +17,23 @@ def scan_ports(target):
         # Create a socket object for TCP connections
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        # Set a timeout for socket connection attempts to avoid long waits
+        # Set a timeout for socket connection attempts to avoid long waits. This time is 1s = 1000ms.
         s.settimeout(1)
 
+        # The 'try' keyword is used to begin a block of code where errors might occur. 
         try:
-            # Attempt to connect to the target IP and port
+            # Attempt to connect to the target IP and port. The 'connect_ex' method is specifically used for
+            # non-blocking socket connections.
             result = s.connect_ex((target, port))
             
             # Check if the connection attempt is successful (result code 0)
             if result == 0:
                 print(f"Port {port}: Open")
+            # If the connection attempt is NOT successful, no message is printed. 
 
         except socket.error as e:
-            # Handle any socket errors that may occur during the connection attempt
+            # Handle any socket errors that may occur during the connection attempt.
+            # Prints an error message including a specific error code.
             print(f"Error while scanning port {port}: {e}")
 
         finally:
